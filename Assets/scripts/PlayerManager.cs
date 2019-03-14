ffabf3c97d45984deafc2c6144d288d5144f46e7
WeenGood <provotorov24@gmail.com>
2019-03-14 23:35:07 +0300

    Added combat

---
+++ Assets/scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject player;
+
+    public void KillPlayer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
